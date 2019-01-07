4c6d39c600918f45fff67cecdb57927d8cd1832a
Qiwei Yang <yangqiwei97@gmail.com>
2019-01-07 23:46:47 +0800

    try to improve speed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
+// app.use(express.static(path.join(__dirname)));
 // app.use("/styles", express.static(__dirname + 'assets/css'));
 // app.use("/images", express.static(__dirname + '/images'));
 // app.use("/scripts", express.static(__dirname + '/assets/js'));
