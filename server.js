0e1a44a6a74936a6e09a5f26c7f0208ac92ebb42
Qiwei Yang <yangqiwei97@gmail.com>
2019-01-07 22:41:29 +0800

    remove redundant code

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("/styles", express.static(__dirname + 'assets/css'));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/scripts", express.static(__dirname + '/assets/js'));
+// app.use("/styles", express.static(__dirname + 'assets/css'));
+// app.use("/images", express.static(__dirname + '/images'));
+// app.use("/scripts", express.static(__dirname + '/assets/js'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
