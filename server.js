3004089b981a278121059fb900641a4868ac177a
Qiwei Yang <yangqiwei97@gmail.com>
2019-01-09 13:07:31 +0800

    add SSL cert

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get('/colorgame', function (req, res) {
   res.sendFile(path.join(__dirname + '/colorGame.html'));
 });
 
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send('http://www.qiweiyang.me/.well-known/acme-challenge/b9D4srFVCgiyhpVah4jLV39syBxhSksW_nwo_2sd6Dg')
+})
 //send email
 app.post('/contact', function (req, res) {
   let mailOpts, smtpTrans; //The route contains my SMTP configuration details, a set of email formatting options I define
@@ -37,10 +40,11 @@ app.post('/contact', function (req, res) {
     }
   });
   mailOpts = {
-    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-    to: 'qw@qiweiyang.me',
+    //from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    from: 'chiwaydyang@gmail.com',
+    to: 'yangqiwei97@gmail.com',
     subject: 'New message from contact form at qiweiyang.me',
-    text: ''
+    text: 'asdf'
     //text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
   };
   smtpTrans.sendMail(mailOpts, function (error, response) {
