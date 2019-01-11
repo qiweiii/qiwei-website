9cbabace8aac51b288124169d5b2eb775b42d6cf
Qiwei Yang <yangqiwei97@gmail.com>
2019-01-11 16:03:35 +0800

    tried to use oauth2 for gmail...

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname + 'assets/css'));
 app.use("/images", express.static(__dirname + '/images'));
 app.use("/scripts", express.static(__dirname + '/assets/js'));
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-GMAIL_USER = process.env.GMAIL_USER;
-GMAIL_PASS = process.env.GMAIL_PASS;
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended: true}));
+// GMAIL_USER = process.env.GMAIL_USER;
+// GMAIL_PASS = process.env.GMAIL_PASS;
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
