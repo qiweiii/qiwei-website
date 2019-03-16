01026b570f4e8ccba52a920ae7bd0696739a8cd6
Qiwei Yang <yangqiwei97@gmail.com>
2019-03-16 21:23:03 +1100

    fix url problem and update resume

--- server.js
+++ server.js
@@ -23,40 +23,12 @@ app.get('/resume', (req, res) => {
 	res.sendFile(path.join(__dirname + '/Resume.pdf'));
 })
 
-// do i still need this?
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
-  res.send('LmCS_e-DNSJJGxd1_CqPIX5CenY_9iuiicANZiMVljQ.SKMMb6gi2eLogAXs8qmH0AN-TKk2fEwHs4DKTb18dtM');
-});
-
-//send email
-// app.post('/contact', function (req, res) {
-//   let mailOpts, smtpTrans; //The route contains my SMTP configuration details, a set of email formatting options I define
-//   smtpTrans = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: GMAIL_USER,
-//       pass: GMAIL_PASS
-//     }
-//   });
-//   mailOpts = {
-//     //from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-//     from: 'chiwaydyang@gmail.com',
-//     to: 'yangqiwei97@gmail.com',
-//     subject: 'New message from contact form at qiweiyang.me',
-//     text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-//   };
-//   smtpTrans.sendMail(mailOpts, function (error, response) {
-//     if (error) {
-//       console.log(error);
-//       res.sendFile(path.join(__dirname + '/index.html'));
-//     } else {
-//       console.log("Message sent: " + response.message);
-//       res.sendFile(path.join(__dirname + '/index.html'));
-//     }
-//   });
+// // do i still need this?
+// app.get('/.well-known/acme-challenge/:content', function(req, res) {
+//   res.send('LmCS_e-DNSJJGxd1_CqPIX5CenY_9iuiicANZiMVljQ.SKMMb6gi2eLogAXs8qmH0AN-TKk2fEwHs4DKTb18dtM');
 // });
+
+
 
 
 
